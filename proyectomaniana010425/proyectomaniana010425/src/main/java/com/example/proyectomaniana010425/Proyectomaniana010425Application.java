d45026205248c77cafdd65fd70d6f5a65d7ef72b
pmorenateide <pmorena@teideformacion.com>
2025-04-01 12:28:53 +0200

    Add files via upload

---
+++ proyectomaniana010425/proyectomaniana010425/src/main/java/com/example/proyectomaniana010425/Proyectomaniana010425Application.java
@@ -0,0 +1,13 @@
+package com.example.proyectomaniana010425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectomaniana010425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectomaniana010425Application.class, args);
+	}
+
+}
---
+++ proyectomaniana010425/proyectomaniana010425/src/test/java/com/example/proyectomaniana010425/Proyectomaniana010425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectomaniana010425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectomaniana010425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
